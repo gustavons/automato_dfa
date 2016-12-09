91d58575bbb1b7fd2c9f5c5c9186a2019ad20151
Gustavo <sougusta@gmail.com>
2016-12-09 00:47:47 -0300

    Programa finalizado

--- ModelAutomato.py
+++ ModelAutomato.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+    Gustavo Nogueira
 
+        Este programa aceita como entrada a Definição	do	alfabeto,
+        Definição	dos	estados, Definição	do	estado	inicial,
+        Definição	dos	estados	finais, Definição	da	função	de	transição e a
+        Lista	de	palavras.
+        O programa informa as quais palavras foram aceitas e quais foram rejeitadas.
+        Informa também o processamento – informando qual o estado	correntea cada símbolo lido.
+
+"""
 
 from ControllerAutomato import automatos
 from ViewAutomato import Saidas
@@ -12,48 +22,34 @@ if __name__ == "__main__":
 
 
     # capturando o conjunto de estados
-    # conjunto_estados = raw_input('Digite os estados:').split(',')
-    conjunto_estados = ['q1','q2','q3', 'q4']
-    # print 'Conjuntos de estados: '
-    # print conjunto_estados
+    conjunto_estados = raw_input('Digite os estados:').split(',')
+
 
     # capturando o alfabeto
-    # alfabeto  = raw_input('Digite o alfabeto: ').split(',')
-    alfabeto = ['a','b']
-    # print 'ALfabeto: '
-    # print alfabeto
+    alfabeto  = raw_input('Digite o alfabeto: ').split(',')
+
 
     # captura da função de transição e processamento
-    # captura_de_funcao = saidas.capturar_funcao_transicao()
-    captura_de_funcao = ['q1,a=q2','q2,b=q3','q3,a=q2', 'q4,a=q3', 'q2,a=q4', 'q2,b=q1', 'q2,b=q4']
-    # funcao_transicao = nfa.tratar_transicao(captura_de_funcao,conjunto_estados, alfabeto)
-    # print 'Funcao de transicao: '
-    # print funcao_transicao
-
+    captura_de_funcao = saidas.capturar_funcao_transicao()
 
 
     # estado inicial
-    # estado_inicial = raw_input('Digite o estado inicial: ')
-    estado_inicial = 'q1'
-    # print 'Estado inicial: '
-    # print estado_inicial
+    estado_inicial = raw_input('Digite o estado inicial: ')
+
 
     # estado final
-    # estado_final =raw_input('Digite o estado final: ')
-    estado_final = 'q4'
-    # print 'Estado final: '
-    # print estado_final
+    estado_final =raw_input('Digite o estado final: ')
+
 
 
     # lista de palavras
-    # lista_palavras = raw_input('Digite uma lista de palavras a serem processadas: ').split(',')
-    lista_palavras = ['aa', 'abaa', 'acc']
-    # print 'Lista de palavras'
-    # print lista_palavras
+    lista_palavras = raw_input('Digite uma lista de palavras a serem processadas: ').split(',')
+
 
     # inicializando o automato
     nfa.iniciar_automato(conjunto_estados,alfabeto,captura_de_funcao,estado_inicial,estado_final, lista_palavras)
 
+    # Saidas de dados para o usuário
     saidas.palavras_aceitas_recusadas(nfa.get_analise())
     saidas.processamento(nfa.get_processamento())
 
